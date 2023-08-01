df82e73c1b6313bbabfd804887c06cbb85f0ee61
haruka.yoshinaga <h.yoshinaga@hikaribf365.onmicrosoft.com>
2023-08-01 03:18:35 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TasksController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//トップページ/をMessagesControllerのindexアクションに設定する
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [TasksController::class, 'index']);
+
+//CRUD taskのページ表示show＞新規登録を処理store＞更新処理update＞削除destroy
+//省略
+Route::resource('tasks', TasksController::class);
